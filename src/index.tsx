2470d33f46397785f1a5659f84ec430f1285672a
Zaporozhtsev Pavel <webzap@proton.me>
2023-12-11 16:26:42 +0300

    Change package json

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,9 @@ import { Counter } from './components/Counter'
 import { render } from 'react-dom'
 
 render(<App />, document.getElementById('root'))
+// "css-loader": "^6.6.0",
+// 		"react": "^17.0.2",
+// 		"react-dom": "^17.0.2",
+// 		"sass": "^1.49.9",
+// 		"sass-loader": "^12.6.0",
+// 		"style-loader": "^3.3.1"
